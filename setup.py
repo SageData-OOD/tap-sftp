26fc37ee0940b09bc0d6f6478ab2656e60069083
Dimitar Petrunov <dimitar.petrunov@sagedata.net>
2022-09-08 15:46:12 +0300

    fix

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setup(
         'backoff==1.8.0',
         'singer-encodings==0.1.1',
         'terminaltables==3.1.0',
-        'bcrypt==3.1.7'
+        'bcrypt==3.1.7',
+        'cryptography==2.7'
     ],
     extras_require={
         'dev': [
